575b1f4fea89a1b74edac8d47a032996f850c88c
UsmanT12 <144747599+UsmanT12@users.noreply.github.com>
2023-09-12 12:45:27 -0700

    fix main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,5 @@
 // main.cpp
 
-// Portions you are to complete are marked with a TODO: comment.
-// We've provided some incorrect return statements (so indicated) just
-// to allow this skeleton program to compile and run, albeit incorrectly.
-// The first thing you probably want to do is implement the trivial
-// functions (marked TRIVIAL).  Then get Arena::display going.  That gives
-// you more flexibility in the order you tackle the rest of the functionality.
-// As you finish implementing each TODO: item, remove its TODO: comment.
 #include "globals.h"
 #include "Robot.h"
 #include "Player.h"
@@ -20,12 +13,11 @@ using namespace std;
 
 int main()
 {
-    // Initialize the random number generator.  (You don't need to
-    // understand how this works.)
+    // Initialize the random number generator
   srand(static_cast<unsigned int>(time(0)));
     
     // Create a game
-    // Use this instead to create a mini-game:   Game g(3, 3, 2);
+    // Use this instead to create a mini-game:   Game g(3, 3, 2)
   Game g(10, 10, 15);
 
     // Play the game
