ba1cfb77047fdd4359f284ad912698f7c952cd7e
UsmanT12 <144747599+UsmanT12@users.noreply.github.com>
2023-09-12 12:47:08 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -18,7 +18,7 @@ int main()
     
     // Create a game
     // Use this instead to create a mini-game:   Game g(3, 3, 2)
-  Game g(10, 10, 15);
+  Game g(10, 15, 100);
 
     // Play the game
   g.play();
